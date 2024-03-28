70c42c16c2dd5ff2eafecbca6faef52805991286
MauritzEmanuel <emanuel.lundbergh@yh.nackademin.se>
2024-03-28 16:22:06 +0100

    Add project files.

---
+++ WebApp/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    let btnMobile = document.querySelector('.btn-mobile')
+    let nav = document.querySelector('nav')
+
+    btnMobile.addEventListener('click', () => {
+        btnMobile.classList.toggle('active')
+        btnMobile.classList.toggle('fixed')
+
+        nav.classList.toggle('active')
+    })
+
+    window.addEventListener('resize', () => {
+        btnMobile.classList.remove('active')
+        btnMobile.classList.remove('fixed')
+
+        nav.classList.remove('active')
+    })
+})
