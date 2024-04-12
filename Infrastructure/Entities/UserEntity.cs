bc39b0120e35bae0b7b2ebd54b58b539c3f1c419
MauritzEmanuel <emanuel.lundbergh@yh.nackademin.se>
2024-04-12 20:40:04 +0200

    Addresses added to databse, not functional

--- Infrastructure/Entities/UserEntity.cs
+++ Infrastructure/Entities/UserEntity.cs
@@ -9,3 +9,18 @@ public class UserEntity : IdentityUser
     [ProtectedPersonalData]
     public string LastName { get; set; } = null!;
 }
+
+public class AddressEntity
+{
+    public int Id { get; set; }
+
+    public string UserId { get; set; } = null!;
+    public UserEntity User { get; set; } = null!;
+
+    public string AddressLine_1 { get; set; } = null!;
+    public string? AddressLine_2 { get; set; }
+    public string PostalCode { get; set; } = null!;
+    public string City { get; set; } = null!;
+
+
+}
