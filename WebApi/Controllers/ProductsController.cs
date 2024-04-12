1720a0f3c6ca6794258576e50c51693348f2a80c
MauritzEmanuel <emanuel.lundbergh@yh.nackademin.se>
2024-04-13 00:34:48 +0200

    Testar SubscribeForm

---
+++ WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok();
+    }
+
+}
