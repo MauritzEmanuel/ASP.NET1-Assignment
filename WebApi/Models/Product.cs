b5c5dfdcfe2d5b07f202d8ca853ddc556d448bea
MauritzEmanuel <emanuel.lundbergh@yh.nackademin.se>
2024-04-18 14:16:05 +0200

    Courses Api Progress

--- WebApi/Models/Product.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WebApi.Models;
-
-public class Product
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = null!;
-}
--- WebApp/Controllers/CoursesController.cs
+++ WebApp/Controllers/CoursesController.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Infrastructure.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
-    public class CoursesController : Controller
+    public class CoursesController(HttpClient http) : Controller
     {
-        public IActionResult Index()
+        private readonly HttpClient _http = http;
+        private string _categoryApiUrl = "https://localhost:7134/api/Categories";
+
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var viewModel = new CoursesViewModel();
+
+            var categoryResponse = await _http.GetAsync(_categoryApiUrl);
+            if (categoryResponse.IsSuccessStatusCode)
+            {
+                var categories = JsonConvert.DeserializeObject<IEnumerable<Category>>(await categoryResponse.Content.ReadAsStringAsync());
+                if (categories != null)
+                    viewModel.Categories = categories;
+            }
+
+
+
+            return View(viewModel);
         }
     }
 }
